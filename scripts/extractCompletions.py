973238183f5fb1885a6aad634e2153254a0a8332
Lanse <simon_stalnabb@hotmail.com>
2017-11-27 16:12:26 +0100

    Major Update
    **Added Support:**
    AbyssalCraft
    Armor Plus
    Armourable Mobs
    Astral Sorcery
    Bad Mobs
    Caravan
    ContentTweaker
    Crossroads
    Dimension Stages
    Extended Crafting
    Horse Power
    Immersive Engineering
    Industrial Foregoing
    Initial Inventory
    Item Stages
    Loot Table Tweaker
    Magneticraft
    Mekanism
    Mob Stages
    Nuclear Craft
    Pneumaticraft Repressurized
    Recipe Stages
    Tech Reborn
    Tinker Stages
    Waila Stages
    
    **Updated Support:**
    ModTweaker

--- scripts/extractCompletions.py
+++ scripts/extractCompletions.py
@@ -4,11 +4,16 @@ import re
 
 
 def fetch_page(url):
-    response = urllib2.urlopen(url)
-    return response.read()
+    try:
+        response = urllib2.urlopen(url)
+        return response.read()
+    except urllib2.HTTPError:
+        return None
 
 
 def param_clean(param):
+    if ' ' not in param and '...' in param:
+        param = param.replace('...', '... ')
     param_splited = param.split(' ')
     param_name = param_splited[-1]
     param_type = param_splited[-2]
@@ -17,6 +22,8 @@ def param_clean(param):
     while param_type.endswith('[]'):
         param_name += '[]'
         param_type = param_type[:-2]
+    if param_type.endswith('...'):
+        param_name += '...'
     return param_name
 
 
@@ -24,12 +31,13 @@ def write_trigger(out, class_name, method_name, params):
     out.write('      { "trigger": "' + method_name + '\\t' + class_name +
               '", "contents": "' + class_name + '.' + method_name + '(' + ', '.join(params) + ');" },\n')
 
-zen_class = re.compile('@ZenClass\("(.*?)"\)')
-zen_method = re.compile('@ZenMethod\n\s+.*?void (.*?)\((.*?)\)')
+zen_class = re.compile('@ZenClass\((value = )?"(.*?)"\)')
+zen_method = re.compile('@ZenMethod\n\s+.*?void (.*?)\s?\((.*?)\)')
 
 out = open('ZenScript.sublime-completions', 'w')
 out.write('{\n   "scope": "plain.text.zs, source.zs",\n\n   "completions":\n   [\n')
 remaps = {}
+errors = []
 with open('remaps.txt') as f:
     for line in f.readlines():
         splitted = line.strip().split(':')
@@ -40,7 +48,10 @@ with open('classes.txt') as f:
         if len(url) is 0 or url.startswith('#'):
             continue
         page = fetch_page(url)
-        class_name = zen_class.search(page).group(1)
+        if page is None:
+            errors.append(url)
+            continue
+        class_name = zen_class.search(page).group(2)
         if class_name in remaps:
             class_name = remaps[class_name]
         methods = re.findall(zen_method, page)
@@ -48,7 +59,11 @@ with open('classes.txt') as f:
         for method in methods:
             method_name = method[0]
             params = method[1].split(', ')
-            params = map(param_clean, params)
+            if params[0] != '':
+                params = map(param_clean, params)
             print '\t' + method_name + '(' + ', '.join(params) + ')'
             write_trigger(out, class_name, method_name, params)
 out.write('   ]\n}')
+print "Errored urls"
+for error in errors:
+    print error
